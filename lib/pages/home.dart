9a58687a5d71b965446625a2b01f4bd7763a5651
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2022-01-18 18:49:56 -0500

    basic layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,22 +8,66 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
 
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Prayer App'),
+        title:
+        Text('prayer app'),
         elevation: 2,
-        leading: Image.asset('assets/images/chewie.png'),
-
-
+        backgroundColor: Colors.deepOrange[300],
+        leading: Image.asset('assets/images/chewie.png'), // REVISIT HOW TO MAKE CIRCULAR
+        leadingWidth: 50,
+        titleTextStyle: TextStyle(
+          fontFamily: 'WallPoet',
+          fontSize: 25,
+        ),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.settings),
-            alignment: Alignment.topLeft ,
+            icon: const Icon(Icons.settings_outlined),
+            alignment: Alignment.center ,
           )
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.teal,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.6),
+        selectedFontSize: 11,
+        unselectedFontSize: 11,
 
+        selectedLabelStyle: TextStyle(
+          fontFamily: 'WallPoet',
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontFamily: 'WallPoet'
+        ),
+        onTap: (value) {
+          // UPDATE TO CHANGE DIRECTION of pressing
+        },
+
+        items:[
+          BottomNavigationBarItem(
+            label: 'Messaging',
+            icon: Icon(Icons.messenger_outline_outlined),
+
+          ),
+          BottomNavigationBarItem(
+            label: 'Salah Times',
+            icon: Icon(Icons.access_time_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: 'Achievements',
+            icon: Icon(Icons.check_sharp),
+          ),
+          BottomNavigationBarItem(
+            label: 'More Info',
+            icon: Icon(Icons.settings_applications),
+          ),
+        ],
       ),
       body: Text('elvis'),
 
