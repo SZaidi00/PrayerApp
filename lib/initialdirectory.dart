6d6231169f3f361e879be7a70262fcf15f78ef7e
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2022-01-21 01:09:05 -0500

    update structure pt1

---
+++ lib/initialdirectory.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:prayerapp/pages/home.dart';
+
+class initialdirectory extends StatelessWidget {
+  const initialdirectory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:prayerapp/initialdirectory.dart';
 import 'package:prayerapp/loading.dart';
 import 'package:prayerapp/pages/home.dart';
 
-void main() {runApp(MaterialApp(
-    initialRoute: '/home',
-    routes:{
-     '/loading': (context) => loading(),
-      //user log in
-      '/home': (context) => Home(),
+void main() => runApp(MyApp());
 
-    }
-));
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: initialdirectory(),
+    );
+  }
 }
 
-
+// I should do: user log in stuff, loading,
