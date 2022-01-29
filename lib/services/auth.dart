b8ead5a0c67d5daf7c4033222d0cde3b8a9887c7
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2022-01-29 13:31:15 -0500

    fixed warnings

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import '../models/myuser.dart';
 
 class AuthService {
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
+// create user object
+  MyUser? _userfromFirebase(User user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
+
 // sign in anon
 Future signInAnon() async {
   try {
 
-   UserCredential result = await _auth.signInAnonymously(); //AuthResult
-   User? user = result.user;                                // FirebaseUser
-    return user;
+    UserCredential result = await _auth.signInAnonymously(); //AuthResult
+    User? user = result.user;                                // FirebaseUser
+    return _userfromFirebase(user!);
+
   } catch (e) {
     print(e.toString());
     return null;
